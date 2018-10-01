80b7d0f70b4e10dcf22f5927d9b1db2e0e9bedd1
Débora Moura <deboramour4@gmail.com>
2018-10-01 11:00:07 -0300

    Select table row

---
+++ AskQuentinhaWatch Extension/GarnishRowController.swift
@@ -0,0 +1,13 @@
+//
+//  GarnishRowController.swift
+//  AskQuentinhaWatch Extension
+//
+//  Created by Ada 2018 on 01/10/2018.
+//  Copyright © 2018 Academy. All rights reserved.
+//
+
+import WatchKit
+
+class GarnishRowController: NSObject {
+
+}
